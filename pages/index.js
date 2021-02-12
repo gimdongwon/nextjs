118df08b21845f8d3d632bf27c1d7ebc2cd5d985
kimdongwon <ehddnjs8989@gmail.com>
2021-02-13 03:31:52 +0900

    [complete]profile layout

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,26 @@ import Link from "next/link"
 
 // import styles from '../styles/Home.module.css'
 import styles from '../styles/index.module.css'
+import Layout from './components/Layout';
 
 export default function Home() {
   return (
     <>
-      <Head><title>Main Page</title></Head>
-      <h1 className={styles.title}>
-        Learn
-        <Link href="https://nextjs.org/">
-          <a>Next.js</a>
-        </Link>
-      </h1>
-      <h2>
-        <Link href="/posts/first-post">
-          <a>My First Post!</a>
-        </Link>
-      </h2>
-      <img src="/vercel.svg" alt="logo"/>
+      <Layout home>
+        <Head><title>Main Page</title></Head>
+        <h1 className={styles.title}>
+          Learn{" "}
+          <Link href="https://nextjs.org/">
+            <a>Next.js</a>
+          </Link>
+        </h1>
+        <h2>
+          <Link href="/posts/first-post">
+            <a>My First Post!</a>
+          </Link>
+        </h2>
+        <img src="/vercel.svg" alt="logo"/>
+      </Layout>
     </>
   )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../components/Layout"
 
 const FirstPost = () => {
   return (
     <>
+        <Layout>
         <Head><title>First Page</title></Head>
         <h1>First post</h1>
-        <h2>
-            <Link href="/">
-                <a>Back to home</a>
-            </Link>
-        </h2>
+        </Layout>
     </>
   )
 };
