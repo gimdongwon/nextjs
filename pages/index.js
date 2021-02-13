b1b8310d597551c84d69ccc6cfe59f7f42b89b28
kimdongwon <ehddnjs8989@gmail.com>
2021-02-13 18:11:48 +0900

    css fixing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from "next/link"
 
-// import utilStyles from "../styles/utils.module.css"
+import utilStyles from "./styles/utils.module.css"
 import Layout from './components/Layout';
 import { getSortedPostsData } from "./lib/posts"
 import Date from "./components/date"
@@ -21,30 +21,30 @@ export default function Home({allPostsData}) {
     <>
       <Layout home>
         <Head><title>Main Page</title></Head>
-        {/* <section className={utilStyles.headingMd}> */}
+        <section className={utilStyles.headingMd}>
            Front-end 지존이 되고 싶습니다.
-           {/* </section> */}
-        {/* <section 
+           </section>
+        <section 
           className={`
           ${utilStyles.headingMd} ${utilStyles.padding1px}
-          `}> */}
-          {/* <h2 className={utilStyles.headingLg}>Blog</h2> */}
-          <ul>
+          `}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
             {allPostsData && allPostsData.map(({ id, date, title }) => (
               <Link href={`posts/${id}`} key={id}>
-               <li> 
+               <li className={utilStyles.listItem}> 
                  {title}
                   <br />
                    {id}
                   <br />
-                  <small>
+                  <small className={utilStyles.lightText}>
                     <Date dateString={date} />
                   </small>
                 </li>
                 </Link>
               ))}
           </ul>
-        {/* </section> */}
+        </section>
       </Layout>
     </>
   )
