4d7e276832034600fd724924e85126bdfba6a5ae
kimdongwon <ehddnjs8989@gmail.com>
2021-02-13 13:02:34 +0900

    [complete]load readme

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ public 파일은 / 밑으로 매칭이 되어 사용이 가능하다.
 css파일의 이름은 무조건 .module.css로 끝나야 한다.
 
 전역 css 설정을 위해 _app.js를 만들고 최상위 컴포넌트로 이것을 이용하여 페이지 간 탐색할 때 상태 유지가 가능하다.
+
+Layout 컴포넌트를 만들고 모든 컴포넌트 상단에 profile Layout 컴포넌트를 넣어줌.
+
+## Pre-rendering and Data Fetching
+
+Next.js를 사용하는 이유는 Pre-rendering을 쉽게 가능하게 해주며 Data Fetching한 내용을 바탕으로 Pre-rendering이 가능하게 된다.
+
+React는 Script를 이용하여 아무것도 없는 상태에서 JS를 이용하여 Dom을 그리는 방식을 채택한다. 하지만 Next.js는 React를 사용하지만 컨텐츠에 대해 HTML을 일부 완성하여 전송하기 때문에 SEO 최적화에 대해 강점을 가지게 된다.
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,46 @@ import Link from "next/link"
 
 // import styles from '../styles/Home.module.css'
 import styles from '../styles/index.module.css'
+import utilStyles from '../styles/utils.module.css'
 import Layout from './components/Layout';
+import { getSortedPostsData } from "./lib/posts"
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+
+export default function Home({allPostsData}) {
+  console.log(allPostsData)
   return (
     <>
       <Layout home>
         <Head><title>Main Page</title></Head>
-        <h1 className={styles.title}>
-          Learn{" "}
-          <Link href="https://nextjs.org/">
-            <a>Next.js</a>
-          </Link>
-        </h1>
-        <h2>
-          <Link href="/posts/first-post">
-            <a>My First Post!</a>
-          </Link>
-        </h2>
-        <img src="/vercel.svg" alt="logo"/>
+        <section className={utilStyles.headingMd}>
+           정보보안 전문가를 꿈꾸고 있는 학생입니다! 
+           </section>
+        <section 
+          className={`
+          ${utilStyles.headingMd} ${utilStyles.padding1px}
+          `}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData && allPostsData.map(({ id, date, title }) => (
+               <li className={utilStyles.listItem} key={id}> 
+                 {title}
+                  <br />
+                   {id}
+                  <br />
+                  {date}
+                </li>
+              ))}
+          </ul>
+        </section>
       </Layout>
     </>
   )
